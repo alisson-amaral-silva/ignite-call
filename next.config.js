d3c01ea211d9502312bf82cab1419f0b431f9b0f
Alisson Amaral <aalissonamaral98@gmail.com>
2023-01-31 20:42:52 -0300

    add configuracao do pageExtensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  // serve para identificar apenas as paginas da aplicacao
+  // o next interpreta td dentro de pages como paginas, por isso precisa dessa config
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 module.exports = nextConfig
