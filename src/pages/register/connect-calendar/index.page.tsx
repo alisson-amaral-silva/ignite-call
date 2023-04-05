9ebc82cd77aaa55b3a335eb2940d67a6a35a471c
Alisson Amaral <aalissonamaral98@gmail.com>
2023-04-04 23:55:13 -0300

    wip - pagina de disponibilidade

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -6,7 +6,7 @@ import { ConnectBox, ConnectItem, AuthError } from './styles'
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 
-export default function Register() {
+export default function ConnectCalendar() {
   // async function handleRegister() {
 
   // }
