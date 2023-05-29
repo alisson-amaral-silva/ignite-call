9f2e1e5efdc63a2158bcc20c51088879d53e76c4
Alisson Amaral <aalissonamaral98@gmail.com>
2023-05-29 20:53:36 -0300

    add estrutura e estilização do calendario

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,10 +1,17 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
   const year = new Date().getFullYear()
   const month = new Date().getMonth()
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(year, month, day))))
     .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toUpperCase()
+      }
       return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
     })
 }
